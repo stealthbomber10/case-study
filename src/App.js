cc4d1829e87db12f23153be6a0d164d04d8bab9e
trentandraka <stealthbomber10@gmail.com>
2018-05-16 13:06:38 -0400

    added test chart, confirmed it works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import ReactChartkick, { LineChart } from 'react-chartkick';
 import logo from './logo.svg';
 import './App.css';
+import Chart from 'chart.js';
+
+ReactChartkick.addAdapter(Chart)
 
 class App extends Component {
   render() {
@@ -13,6 +17,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <LineChart data={{"2017-01-01": 11, "2017-01-02": 6}} />
       </div>
     );
   }
