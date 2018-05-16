3a24501795ce76c54193677772c5bb8f95da9527
trentandraka <stealthbomber10@gmail.com>
2018-05-16 16:57:05 -0400

    skeleton for stock ticker class

--- src/component/StockTicker.js
+++ src/component/StockTicker.js
@@ -1,9 +1,35 @@
-import React from 'react'; 
-function StockTicker() { 
-return(
-    <div class = 'stock-ticker'> Stock Ticker: <input type= 'text' id = 'sTicker'/> </div>
+import React from 'react';
 
-); 
-} 
+class StockTicker extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    handleChange(event) {
+        if (event.length > 0) {
+            // get rest data and set it in ticker
+        }
+        else {
+            this.props.onChange(undefined);
+        }
+    }
+
+    render() {
+        return (
+            <div class = 'stock-ticker'> Stock Ticker: <input type= 'text' id = 'sTicker'/>
+            </div>
+        );
+    }
+}
+
+// function StockTicker() { 
+// return(
+//     <div class = 'stock-ticker'> Stock Ticker: <input type= 'text' id = 'sTicker'/> </div>
+
+// ); 
+// } 
 
 export default StockTicker; 
