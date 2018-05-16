208055020274554f1c7c7b80f6c6e4e41838494f
Elio Lugo <eliolugo@Elios-MacBook-Pro.local>
2018-05-16 12:47:14 -0400

    changes

--- src/component/StartDate.js
+++ src/component/StartDate.js
@@ -2,8 +2,11 @@ import React from 'react';
 function StartDate(){
     return(     
         <div> Start Date: <input type= 'text'/> </div>
+
     ); 
     } 
     
     export default StartDate; 
 
+
+    
